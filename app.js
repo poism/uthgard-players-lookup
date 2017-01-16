4e6263964967c430da56a420af040528028103ea
Sherab Sangpo Dorje <po@poism.com>
2017-01-16 14:30:03 -0500

    bugfix sanitize debounce, allow appending search results, remove results that have already been found

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /* Uthgard Players Lookup Tool
  * By Sherab Sangpo Dorje ( po@poism.com )
- * 
+ *  
 */
 
 window.onload = function () {
@@ -8,61 +8,23 @@ window.onload = function () {
         el: '#uthgard-players',
         data: {
             debug: false,
-            fakeapi: true,
+            fakeapi: false,
             apiURL: 'api.php?names=',
             defaultNames: 'Mistar,Bruno,Ascerian,Felrith',
             namesInput: '',
             namesInputSanitized: '',
             namesInputArray: [],
+            namesNotFound: [],
             inProgress: false,
             hasResults: false,
+            results: [],
             users: []
         },
-        watch: {
-            namesInput: function () {
-                if ( this.namesInput !== this.namesInputSanitized ){
-                    this.sanitizeInput()
-                }
+        computed: {
+            usersByExperience: function () {
+                var self = this
+                return _.orderBy(self.users, ['Raw.XP', 'RealmRank', 'RP_Percent'],['desc', 'desc', 'desc'])
             }
-        },
-        methods: {
-            toggleDebug: function () {
-                this.debug = !this.debug
-            },
-            fetchAllNames: function () {
-                if ( this.namesInputSanitized == '' ) {
-                    this.namesInput = this.defaultNames
-                }
-                this.sanitizeInput()
-                this.fetchData()
-            },
-            fetchData: function (name) {
-              var xhr = new XMLHttpRequest()
-              var self = this
-              self.inProgress = true
-              xhr.open('GET', encodeURI(self.apiURL + self.namesInput + (self.fakeapi ? '&fakeapi' : '')) )
-              xhr.onload = function () {
-                self.users = JSON.parse(xhr.responseText)
-                self.inProgress = false
-                self.hasResults = true
-                console.log(self.users)
-              }
-              xhr.send()
-            },
-            sanitizeInput: _.debounce(
-                function () {
-                    this.namesInputSanitized = this.namesInput.replace(/,{2,}/g, ",")
-                    this.namesInputArray = this.namesInputSanitized.split(',')
-                    for (var i = 0, len = this.namesInputArray.length; i < len; i++) {
-                        this.namesInputArray[i] = this.namesInputArray[i].trim()
-                        this.namesInputArray[i] = this.namesInputArray[i].replace(/[^a-zA-Z ]/g, "")
-                        this.namesInputArray[i] = this.namesInputArray[i].charAt(0).toUpperCase() + this.namesInputArray[i].substr(1).toLowerCase()
-                    }
-                    this.namesInputSanitized = this.namesInputArray.join(',')
-                    this.namesInput = this.namesInputSanitized
-                    return
-                }, 500
-                )
         },
         filters: {
             formatTimestamp: function (timestamp) {
@@ -78,11 +40,76 @@ window.onload = function () {
                 return (date.toUTCString())
             }
         },
-        computed: {
-            usersByExperience: function () {
-                var self = this
-                return _.orderBy(self.users, ['Raw.XP', 'RealmRank', 'RP_Percent'],['desc', 'desc', 'desc'])
+        watch: {
+            namesInput: function () {
+                if ( this.namesInput !== this.namesInputSanitized ){
+                    this.sanitizeInputOnChange()
+                }
             }
+        },
+        methods: {
+            toggleDebug: function () {
+                this.debug = !this.debug
+            },
+            fetchAllNames: function () {
+                var self = this
+                
+                if ( self.namesInputSanitized == '' ) {
+                    self.namesInput = self.defaultNames
+                }
+                self.namesInputArray = []
+                self.results = []
+                self.fetchData()
+            },
+            fetchData: function (name) {
+              var xhr = new XMLHttpRequest()
+              var self = this
+              self.inProgress = true
+              self.sanitizeInput()
+              console.log('Fetching: ' + self.namesInput)
+              xhr.open('GET', encodeURI(self.apiURL + self.namesInput + (self.fakeapi ? '&fakeapi' : '')) )
+              xhr.onload = function () {
+                self.results = JSON.parse(xhr.responseText)
+                for (var i = 0, len = self.results.length; i < len; i++) {
+                    if ( self.results[i] === null ) {
+                        self.namesNotFound.push( self.namesInputArray[i] )
+                        console.log('Fetching: '+ self.namesInputArray[i] + ' not found')
+                    }
+                    else {
+                        self.users.push( self.results[i] )
+                        console.log('Fetching: ' + self.namesInputArray[i] + ' found')
+                    }
+                }
+                console.log(self.users)
+                console.log('Names not found: ' + self.namesNotFound.join(','))
+                self.inProgress = false
+                self.hasResults = true
+              }
+              xhr.send()
+            },
+            sanitizeInput: function() {
+                    var self = this
+                    self.namesInputSanitized = self.namesInput.replace(/,{2,}/g, ",")
+                    self.namesInputArray = self.namesInputSanitized.split(',')
+                    for (var i = 0, len = self.namesInputArray.length; i < len; i++) {
+                        self.namesInputArray[i] = self.namesInputArray[i].trim()
+                        self.namesInputArray[i] = self.namesInputArray[i].replace(/[^a-zA-Z ]/g, "")
+                        self.namesInputArray[i] = self.namesInputArray[i].charAt(0).toUpperCase() + self.namesInputArray[i].substr(1).toLowerCase()
+                        console.log('sanitizeInput: ' + self.namesInputArray[i])
+                        for (var r = 0, rlen = self.users.length; r < rlen; r++) {
+                            if ( self.users[r].Raw.Name == self.namesInputArray[i] ) {
+                                console.log('Omitting "' + self.namesInputArray[i] + '" from query since it already exists.')
+                                self.namesInputArray.splice(i, 1)
+                                len = len - 1
+                            }
+                        }
+                    }
+                    self.namesInputSanitized = self.namesInputArray.join(',')
+                    self.namesInput = self.namesInputSanitized
+            },
+            sanitizeInputOnChange: _.debounce(function () {
+                    this.sanitizeInput()
+                }, 500)
         }
     });
 }
